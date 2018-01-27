92644588af497b5a246942a6340936834253c1d3
zhangweixin <357448012@qq.com>
2018-01-27 14:26:41 +0800

    webpack babel gitignore

---
+++ webpack.dev.config.js
@@ -0,0 +1,19 @@
+
+const path = require("path");
+
+module.exports ={
+    entry:path.join(__dirname,"./src/index.js"),
+    output: {
+        path: path.join(__dirname,"./dist"),
+        filename: '[hash]-bundle.js'
+    },
+    module: {
+        rules:[
+            {
+                test: /\.js$/,
+                use: ["babel-loader?cacheDirectory=true"],
+                include: path.join(__dirname,"./src")
+            }
+        ]
+    }
+}
