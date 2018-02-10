71d9f17d72ef8d102c419275fcce4e8c0002043b
zhangweixin <zhangweixin@zuzuche.com>
2018-02-10 15:06:19 +0800

    vender && axios

--- webpack.config.js
+++ webpack.config.js
@@ -66,7 +66,14 @@ module.exports ={
         }),
         new webpack.optimize.CommonsChunkPlugin({
             name: 'vendor',
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
             name: 'runtime'
+            //, name: 'manifest'  这里的name好像可以随便改，可以保证vendorhash不变，
+            // https://doc.webpack-china.org/guides/caching
+            //main bundle 会随着自身的新增内容的修改，而发生变化。
+            //vendor bundle 会随着自身的 module.id 的修改，而发生变化。
+            //manifest bundle 会因为当前包含一个新模块的引用，而发生变化。
         }),
         new uglifyJSPlugin(),
         new webpack.DefinePlugin({
@@ -78,7 +85,13 @@ module.exports ={
          new cleanWebpackPlugin(['dist']),
          new extractTextWebpackPugin({
              filename: '[name].[contenthash:5].css',
-             allChunks: true
+             allChunks: true,
+             //从所有额外的 chunk(additional chunk) 提取（默认情况下，
+             //它仅从初始chunk(initial chunk) 中提取）
+            //当使用 CommonsChunkPlugin 并且在公共 chunk 中有提取的 chunk
+            //（来自ExtractTextPlugin.extract）时，allChunks **必须设置为 true
+
+            // chunk type 块的类型大揭秘 https://github.com/ShowJoy-com/showjoy-blog/issues/7
          })
    ]
 }
