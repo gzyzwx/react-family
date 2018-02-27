49a3768560498196036ea4888184f9320ea09496
zhangweixin <zhangweixin@zuzuche.com>
2018-02-27 12:11:42 +0800

    server

--- server.js
+++ server.js
@@ -1,21 +1,14 @@
 var express = require('express');
 var app = express();
 var history = require('connect-history-api-fallback');
-
-// var router = express.Router();
-
+let prot = 3000;
 app.use(history());
-// router.get('/', function (req, res, next) {
-//     req.url = '/index.html';
-//     next();
-// });
 
-// app.use(router);
 app.use(express.static('./dist'));
-module.exports = app.listen(3000,function (err) {
+module.exports = app.listen(prot,function (err) {
     if (err) {
         console.log(err);
         return
     }
-    console.log('Listening at http://localhost:' + 3000 + '\n')
-})
+    console.log('Listening at http://localhost:' + prot + '\n')
+});
